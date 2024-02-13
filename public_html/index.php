f58624a34bd808139e09c213b7a4a922b722ac2f
calcifer <olivier.cote2@hotmail.ca>
2024-02-13 09:59:14 -0500

    Partie 1D terminé Clean-up à faire

--- public_html/index.php
+++ public_html/index.php
@@ -7,11 +7,10 @@ require_once 'php/classes/Editeur.php';
 require_once 'php/classes/Difficulte.php';
 require_once 'php/bd.php';
 
-use Cegep\Web4\GestionJeu\{Jeu, Repertoire};
-
 $gamesData = getTousLesJeux();
-//usort($games, fn($a, $b) => $a->getTitre() <=> $b->getTitre());
- ?>
+$liste = $gamesData->getJeux();
+usort($liste, fn($a, $b) => strnatcasecmp($a->getTitre(), $b->getTitre()));
+?>
 <!doctype html>
 <html lang="fr">
 <head>
@@ -27,35 +26,34 @@ $gamesData = getTousLesJeux();
 <div class="header">
     <?php
     if (!isset($_SESSION['estConnecte'])) {
-    ?>
+        ?>
         <a href="php/pages/login.php"> Se connecter</a>
-    <?php
+        <?php
     } else {
-    ?>
+        ?>
         <div>
             <a href="php/pages/ajoutEditeur.php">Ajouter un éditeur</a>
             <a href="php/pages/ajoutJeu.php">Ajouter un jeu</a>
             <a href="php/pages/logout.php">Déconnecter</a>
         </div>
-    <?php
+        <?php
     }
     ?>
 </div>
 <h1>Sommatif #1</h1>
 <ul id="liste-jeux">
     <?php
-    if (!empty($gamesData)) {
-    foreach ($gamesData->getJeux() as $game) {
-        var_dump(getEditeurBd($game->getEditeur()->nom));
-    ?>
-    <li class="difficulte-<?= $game->getDifficulte()->value ?>">
-        <p class="titre-jeu"><?= $game->getTitre() ?></p>
-        <p class="editeur-jeu"><?= getEditeurBd($game->getEditeur()->nom) ?></p>
-    </li>
-    <?php
-    }
+    if (!empty($liste)) {
+        foreach ($liste as $game) {
+            ?>
+            <li class="difficulte-<?= $game->getDifficulte()->value ?>">
+                <p class="titre-jeu"><?= $game->getTitre() ?></p>
+                <p class="editeur-jeu"><?= getEditeurBd($game->getEditeur()->nom) ?></p>
+            </li>
+            <?php
+        }
     } else {
-    echo "<h1>Aucun jeu pour le moment...</h1>";
+        echo "<h1>Aucun jeu pour le moment...</h1>";
     } ?>
 </ul>
 </html>
